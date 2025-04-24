b037cbb6a546eb426c8353e08ab9ed5ec52ba898
nimra-iqbal512 <nimraiqbal512@gmail.com>
2025-04-25 00:39:44 +0500

    readData

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {database} from './config'
-import { addDoc, collection } from 'firebase/firestore'
+import { addDoc, getDocs, collection } from 'firebase/firestore'
 
 const App = () => {
   const [fname, setFname] = useState('')
   const [lname, setLname] = useState('')
+
+  const [val, setVal] = useState([])
+
+  // 'value' below contains the details of database
   const value = collection(database, 'FirebaseFirestoreReact')  //Provide name of database
+
+  useEffect( ()=>{
+    const getData = async()=>{
+      const dbVal = await getDocs(value)
+      // console.log(dbVal);
+      
+      setVal(dbVal.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+      }) ))
+    }
+    getData()
+  })
 
   const handleCreate = async()=>{
     //Adding data to Firebase
@@ -25,6 +42,13 @@ const App = () => {
         onChange={(e) => setLname(e.target.value)}
       />
       <button onClick={handleCreate}>Create</button>
+
+      {val.map((values) => (
+        <div>
+          <h1>{values.fname}</h1>
+          <h1>{values.lname}</h1>
+        </div>
+      ))}
     </div>
   );
 }
