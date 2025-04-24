cd0299e5573595669308411081e8ed4b4b513987
nimra-iqbal512 <nimraiqbal512@gmail.com>
2025-04-24 09:45:59 +0500

    setup

---
+++ src/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqAZSC1eTpe8UmdvdGH7DRGAgnlTWp0a8",
+  authDomain: "fir-firestorereact.firebaseapp.com",
+  projectId: "fir-firestorereact",
+  storageBucket: "fir-firestorereact.firebasestorage.app",
+  messagingSenderId: "53563829795",
+  appId: "1:53563829795:web:a326bfff8cfecad5720b96",
+}; 
+
+const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
